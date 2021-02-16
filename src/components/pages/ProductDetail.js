a4c71f45889fac14cac8020c29babeee44dd97a4
Mauricio <maur.martins32@gmail.com>
2021-02-16 17:01:14 -0300

    Add routes

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FaIdCard, FaHome, FaScroll } from "react-icons/fa";
 
+import { useScrollToTop } from "hooks/scroll";
+
 import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
 import Section from "components/molecules/Section";
@@ -13,6 +15,7 @@ import Callout, {
   CalloutMedia,
 } from "components/atoms/Callout";
 import Button from "components/atoms/Button";
+import BreadCrumb from "components/atoms/BreadCrumb";
 
 import SpeedImage from "draws/Speed";
 import HeroImage from "assets/hero.jpg";
@@ -34,76 +37,85 @@ const PinnedItem = styled.li`
   }
 `;
 
-const ProductDetail = () => (
-  <>
-    <Hero image={HeroImage}>
-      <Heading>
-        <h1>Nome do serviço</h1>
-      </Heading>
-    </Hero>
-    <Section>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab corporis,
-        pariatur asperiores ipsum unde, quos in eaque quisquam repellat cumque
-        quibusdam odit! Ipsam amet quae repudiandae illo adipisci nesciunt
-        impedit.
-      </p>
-      <p>
-        Aut nemo nisi, voluptatem veritatis ipsa cumque ratione non, et velit a,
-        id accusantium! Adipisci, nemo, dolore architecto ab harum, doloremque
-        atque corporis hic culpa necessitatibus tempore obcaecati impedit
-        molestias.
-      </p>
-      <p>
-        Tempora corporis alias odit dolor aut mollitia fugiat ratione. Accusamus
-        veritatis modi, quisquam quibusdam recusandae minima, minus est facere
-        cumque hic voluptatum possimus dignissimos nulla quam mollitia ut
-        temporibus accusantium!
-      </p>
+const ProductDetail = () => {
+  useScrollToTop();
+  return (
+    <>
+      <Hero image={HeroImage}>
+        <Heading>
+          <h1>Nome do serviço</h1>
+          <BreadCrumb
+            items={[
+              { label: "Início", link: "/" },
+              { label: "Serviços" },
+              { label: "Nome de Serviço" },
+            ]}
+          />
+        </Heading>
+      </Hero>
+      <Section>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab corporis,
+          pariatur asperiores ipsum unde, quos in eaque quisquam repellat cumque
+          quibusdam odit! Ipsam amet quae repudiandae illo adipisci nesciunt
+          impedit.
+        </p>
+        <p>
+          Aut nemo nisi, voluptatem veritatis ipsa cumque ratione non, et velit
+          a, id accusantium! Adipisci, nemo, dolore architecto ab harum,
+          doloremque atque corporis hic culpa necessitatibus tempore obcaecati
+          impedit molestias.
+        </p>
+        <p>
+          Tempora corporis alias odit dolor aut mollitia fugiat ratione.
+          Accusamus veritatis modi, quisquam quibusdam recusandae minima, minus
+          est facere cumque hic voluptatum possimus dignissimos nulla quam
+          mollitia ut temporibus accusantium!
+        </p>
 
-      <h5>Documentos necessários</h5>
-      <PinnedList>
-        <PinnedItem>
-          <FaIdCard />
-          RG
-        </PinnedItem>
-        <PinnedItem>
-          <FaIdCard />
-          CPF
-        </PinnedItem>
-        <PinnedItem>
-          <FaScroll />
-          Certidão de nascimento ou casamento
-        </PinnedItem>
-        <PinnedItem>
-          <FaHome />
-          Comprovante de residência
-        </PinnedItem>
-      </PinnedList>
-    </Section>
-    <Section inverse>
-      <Callout>
-        <CalloutBody>
-          <h6>Faça sua matrícula agora mesmo</h6>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-            quis libero quo sapiente nobis asperiores dolores? Eius, dolores
-            officiis! Quia obcaecati nam, corporis nobis ex dolore porro minus!
-            Tempora, quisquam!
-          </p>
-          <CalloutActions>
-            <Button color="primary">Matricular</Button>
-          </CalloutActions>
-        </CalloutBody>
-        <CalloutMedia>
-          <SpeedImage />
-        </CalloutMedia>
-      </Callout>
-    </Section>
-    <Footer />
-  </>
-);
-
+        <h5>Documentos necessários</h5>
+        <PinnedList>
+          <PinnedItem>
+            <FaIdCard />
+            RG
+          </PinnedItem>
+          <PinnedItem>
+            <FaIdCard />
+            CPF
+          </PinnedItem>
+          <PinnedItem>
+            <FaScroll />
+            Certidão de nascimento ou casamento
+          </PinnedItem>
+          <PinnedItem>
+            <FaHome />
+            Comprovante de residência
+          </PinnedItem>
+        </PinnedList>
+      </Section>
+      <Section inverse>
+        <Callout>
+          <CalloutBody>
+            <h6>Faça sua matrícula agora mesmo</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              quis libero quo sapiente nobis asperiores dolores? Eius, dolores
+              officiis! Quia obcaecati nam, corporis nobis ex dolore porro
+              minus! Tempora, quisquam!
+            </p>
+            <CalloutActions>
+              <Button color="primary">Matricular</Button>
+            </CalloutActions>
+          </CalloutBody>
+          <CalloutMedia>
+            <SpeedImage />
+          </CalloutMedia>
+        </Callout>
+      </Section>
+      <Footer />
+    </>
+  );
+};
 ProductDetail.defaultProps = {};
 
 ProductDetail.propTypes = {};
